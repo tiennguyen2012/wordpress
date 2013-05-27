544184485e71937286042b4277a1d5846cc47662
Tien Nguyen <tien.nguyen@kiss-concept.com>
2013-05-27 10:40:28 +0700

    Fresh project

--- wp-content/plugins/wp-e-commerce-store-toolkit/templates/admin/wpsc-admin_st_costprice_38.php
+++
@@ -1,2 +0,0 @@
-<label for="wpsc_st_cost_price"><?php _e( 'Cost Price', 'wpsc_st' ); ?>:</label><br />
-<input type="text" id="wpsc_st_cost_price" name="meta[_wpsc_cost_price]" size="10" class="text" value="<?php echo $cost_price; ?>" />
